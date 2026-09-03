bdcdf133e2efdecf192e94703b5b074f41d89c29
Fatimakochkarova <kochkarovafatima@gmail.com>
2026-09-03 13:37:57 +0300

    homework-10

---
+++ products.js
@@ -0,0 +1,62 @@
+const cosmeticsProducts = [
+  {
+    id: 1,
+    skinType: "ДЛЯ НОРМАЛЬНОЙ КОЖИ",
+    title: "Увлажняющий мусс",
+    description: "Глубоко увлажняет кожу лица, оставляя её мягкой и гладкой.",
+    composition: ["активные натуральные комплексы", "витамины C, A, PP, B и E", "солнцезащитные компоненты"],
+    price: 2750,
+    currency: "₽",
+    imageUrl: "./images/photo_1.jpg",
+    isRecommended: false
+  },
+  {
+    id: 2,
+    skinType: "ДЛЯ НОРМАЛЬНОЙ КОЖИ",
+    title: "Увлажняющая маска",
+    description: "Способствует удерживанию влаги в верхних слоях кожи.",
+    composition: ["воски", "минералы", "масла"],
+    price: 3500,
+    currency: "₽",
+    imageUrl: "./images/photo_2.jpg",
+    isRecommended: false
+  },
+  {
+    id: 3,
+    skinType: "ДЛЯ НОРМАЛЬНОЙ КОЖИ",
+    title: "Гель для умывания",
+    description: "Интенсивно очищает, не повреждая защитный барьер кожи.",
+    composition: ["минералы", "витамины C, A, PP, B и E", "солнцезащитные компоненты"],
+    price: 1650,
+    currency: "₽",
+    imageUrl: "./images/photo_3.jpg",
+    isRecommended: false
+  },
+  {
+    id: 4,
+    skinType: "ДЛЯ НОРМАЛЬНОЙ КОЖИ",
+    title: "Подарочный набор №1",
+    description: "Набор, состоящий из увлажняющего крема и маски.",
+    composition: ["воски", "минералы", "масла"],
+    price: 4750,
+    currency: "₽",
+    imageUrl: "./images/photo_4.jpg",
+    isRecommended: false
+  },
+  {
+    id: 5,
+    skinType: "ДЛЯ НОРМАЛЬНОЙ КОЖИ",
+    title: "Подарочный набор №5",
+    description: "Весь набор средств Invisible Symphony: крем, маска, мусс и гель для умывания.",
+    composition: ["воски", "минералы", "масла"],
+    price: 7520,
+    currency: "₽",
+    imageUrl: "./images/photo_5.jpg",
+    isRecommended: false
+  }
+];
+
+export default cosmeticsProducts;
+
+
+
